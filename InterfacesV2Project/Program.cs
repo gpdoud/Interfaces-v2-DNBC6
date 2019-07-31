95eabb5c77ca72e768e5f46964eb37425bd893ff
Greg Doud <gpdoud@gmail.com>
2019-07-31 10:50:17 -0400

    Initial commit

--- InterfacesV2Project/Program.cs
+++ InterfacesV2Project/Program.cs
@@ -6,6 +6,14 @@ namespace InterfacesV2Project {
 
         static void Main(string[] args) {
 
+            var p1 = new Product(1, "Basic Widget", 10, 2500);
+            var p2 = new Product(2, "Advanced Widget", 25, 1500);
+            var p3 = new Product(3, "Supreme Widget", 50, 1000);
+
+            var s1 = new Service(4, "Basic Service", 25, 1000);
+            var s2 = new Service(5, "Priority Service", 50, 550);
+            var s3 = new Service(6, "24x7 Service", 100, 750);
+
         }
     }
 }
